1e617ceefaa6c6ac10203e0558a63d37454de62f
aetana <amanuel.etanan@gmail.com>
2023-03-22 17:21:26 -0500

    helper frist commit

---
+++ src/main/java/scoringhelper/ScoringHelper/ScoringHelperApplication.java
@@ -0,0 +1,13 @@
+package scoringhelper.ScoringHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoringHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoringHelperApplication.class, args);
+	}
+
+}
